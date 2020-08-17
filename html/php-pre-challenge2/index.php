2d6c377f82f604e3ddcce9266ea8671aae82d6da
takumi <takumik9823@gmail.com>
2020-08-18 06:11:08 +0900

    練習課題2 完成

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,10 +3,18 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+  for ($j = 1; $j < count($array); $j++) {
+    if ($array[$j-1] > $array[$j]) {
+      $result = $array[$j];
+      $array[$j] = $array[$j-1];
+      $array[$j-1] = $result;
+    }
+  }
 }
 // 修正はここまで
 
 echo "<pre>";
 print_r($array);
 echo "</pre>";
+
+
