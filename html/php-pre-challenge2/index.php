0ddb195e39a8439bc4c3be2f725c44e0e1ce2e9e
takumi <takumik9823@gmail.com>
2020-08-21 12:43:19 +0900

    練習課題2　タイポ修正

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,8 +3,8 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 $lenght = count($array);
-for ($i = 0; $i < $length - 1; $i++) {
-  for ($j = 1; $j < $length -$i; $j++) {
+for ($i = 0; $i < $lenght - 1; $i++) {
+  for ($j = 1; $j < $lenght - $i; $j++) {
     if ($array[$j-1] > $array[$j]) {
       $result = $array[$j];
       $array[$j] = $array[$j-1];
