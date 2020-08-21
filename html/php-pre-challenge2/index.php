b8a3c0ead19bd62d4976339f8300005de97cae07
takumi <takumik9823@gmail.com>
2020-08-21 14:14:29 +0900

    練習課題2　スペルミス修正

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,9 +2,9 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-$lenght = count($array);
-for ($i = 0; $i < $lenght - 1; $i++) {
-  for ($j = 1; $j < $lenght - $i; $j++) {
+$length = count($array);
+for ($i = 0; $i < $length - 1; $i++) {
+  for ($j = 1; $j < $length - $i; $j++) {
     if ($array[$j-1] > $array[$j]) {
       $result = $array[$j];
       $array[$j] = $array[$j-1];
