2a13e9f44754cd90a252ef4b2d2263d891c22a7d
takumi <takumik9823@gmail.com>
2020-08-21 10:10:20 +0900

    練習課題1の削除

--- html/php-pre-challenge1/index.php
+++ html/php-pre-challenge1/index.php
@@ -1,15 +1,6 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
     //ここからコードを書く
-    if($i % 3 === 0) {
-        $result = '3の倍数';
-        if($i % 5 === 0){
-          $result = '3の倍数であり、5の倍数';
-        }
-    }elseif($i % 5 === 0){
-        $result = '5の倍数';
-    }else{
-        $result = $i;
-    }
-    echo ($result.'<br>');
+    
+    echo ($i);
 }
