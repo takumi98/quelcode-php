d6d0c7f8d57d416fc28d14f61583763aa3ecc10b
takumi <takumik9823@gmail.com>
2020-09-07 18:51:34 +0900

    テーブル構造の変更

--- mysql5.7/init.sql
+++ mysql5.7/init.sql
@@ -14,10 +14,16 @@ CREATE TABLE `posts` (
       `message` text NOT NULL,
       `member_id` int(11) NOT NULL,
       `reply_post_id` int(11) NOT NULL,
+      `retweet_message_id` int(11) NULL,
       `created` datetime NOT NULL,
       `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
       PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE `like` (
+      `like_user_id` int(11) NOT NULL,
+      `like_message_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `prechallenge3` (
       `value` int(11) NOT NULL
