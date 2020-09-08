c200188ce464f7df0b9708acc45d1ffa70b8ae20
takumi <takumik9823@gmail.com>
2020-09-08 14:33:21 +0900

    不必要な条件式を削除

--- html/php-challenge/delete.php
+++ html/php-challenge/delete.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['id'])) {
 			$_SESSION['id'],
 			$id
 		));
-	} elseif ($message['retweet_message_id'] != null && $_SESSION['id'] != $message['member_id']){// 他のユーザーにRTされた記事
+	} elseif ($_SESSION['id'] != $message['member_id']){// 他のユーザーにRTされた記事
 		$del = $db->prepare('DELETE FROM posts WHERE member_id=? AND retweet_message_id=?');
 		$del->execute(array(
 			$_SESSION['id'],
